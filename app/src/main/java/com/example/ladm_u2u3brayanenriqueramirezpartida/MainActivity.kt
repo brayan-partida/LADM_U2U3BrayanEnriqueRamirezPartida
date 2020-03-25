0f9f49fb62560b5f8a4d21328ab9a70767a97845
brayan-partida <brenramirezpa@ittepic.edu.mx>
2020-03-24 22:57:35 -0800

    Initial commit

---
+++ app/src/main/java/com/example/ladm_u2u3brayanenriqueramirezpartida/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ladm_u2u3brayanenriqueramirezpartida
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
